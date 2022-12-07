4d1e50428a7f6ec5e2d2a352d38239d2843ea9da
Mateusz <matsoj01@gmail.com>
2022-12-07 20:53:35 +0000

    mobile version footer created

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,18 @@
 const navBtn = document.querySelector(".navBtn");
 const nav = document.querySelector(".nav");
+const footerYear = document.querySelector(".footer__year");
 
 const showNav = () => {
 	nav.classList.toggle("showNav");
 };
+
+
+const handleCurrentYear = () => {
+    const year = new Date().getFullYear();
+    footerYear.innerText = year;
+}
+
+
+handleCurrentYear()
 navBtn.addEventListener("click", showNav);
+
