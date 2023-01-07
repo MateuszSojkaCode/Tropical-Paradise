34dbc22c25791849722a1c4bad489087244089da
Mateusz <matsoj01@gmail.com>
2023-01-07 15:05:59 +0000

    added function that changes color of menu button on different sections

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,13 @@
 const navBtn = document.querySelector(".navBtn");
 const nav = document.querySelector(".nav");
 const footerYear = document.querySelector(".footer__year");
+const navBtnBars = document.querySelector('.navBtn__line');
+const allSections = document.querySelectorAll('.section')
 
 const showNav = () => {
 	nav.classList.toggle("showNav");
+
+    navBtnBars.classList.remove('black-bars-color')
 };
 
 
@@ -12,7 +16,20 @@ const handleCurrentYear = () => {
     footerYear.innerText = year;
 }
 
+const observerSection = () => {
+    const currentSection = window.scrollY
+
+    allSections.forEach(section => {
+        if(section.classList.contains('white-section') && section.offsetTop <= currentSection + 60) {
+            navBtnBars.classList.add('black-bars-color')
+        } else if (!section.classList.contains('white-section') && section.offsetTop <= currentSection + 60){
+            navBtnBars.classList.remove('black-bars-color')
+        }
+    })
+}
+
+
 
 handleCurrentYear()
 navBtn.addEventListener("click", showNav);
-
+window.addEventListener('scroll', observerSection)
