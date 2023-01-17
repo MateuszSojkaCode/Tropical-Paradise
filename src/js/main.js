b637b49b6724e5a26f2038de7a3dc43871460858
Mateusz <matsoj01@gmail.com>
2023-01-17 20:11:56 +0000

    created section reviews

--- src/js/main.js
+++ src/js/main.js
@@ -3,12 +3,8 @@ const nav = document.querySelector(".nav");
 const footerYear = document.querySelector(".footer__year");
 const navBtnBars = document.querySelector('.navBtn__line');
 const allSections = document.querySelectorAll('.section')
+const allNavItems = document.querySelectorAll('.nav__link')
 
-const showNav = () => {
-	nav.classList.toggle("showNav");
-
-    navBtnBars.classList.remove('black-bars-color')
-};
 
 
 const handleCurrentYear = () => {
@@ -18,7 +14,7 @@ const handleCurrentYear = () => {
 
 const observerSection = () => {
     const currentSection = window.scrollY
-
+    
     allSections.forEach(section => {
         if(section.classList.contains('white-section') && section.offsetTop <= currentSection + 60) {
             navBtnBars.classList.add('black-bars-color')
@@ -28,6 +24,17 @@ const observerSection = () => {
     })
 }
 
+const showNav = () => {
+    nav.classList.toggle("showNav");
+
+    navBtnBars.classList.remove('black-bars-color')
+
+    allNavItems.forEach(item => {
+        item.addEventListener('click', () => {
+            nav.classList.remove('showNav')
+        })
+    })
+};
 
 
 handleCurrentYear()
